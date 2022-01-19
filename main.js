a78323d44e6e75acd5afa20803c793fcfba294ce
Yashashvi678 <81162244+Yashashvi678@users.noreply.github.com>
2022-01-19 13:28:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,18 +10,22 @@ function preload()
  
 function setup()
 {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(380, 380);
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML= "Objects Detecting";
 }
  
+
 function modelLoaded()
 {
-    console.log("modelLoaded");
+    console.log("Model Loaded");
     status = true;
-    objectDetector.detect(img, gotResult);
 }
+ 
  
 function gotResult(error, results)
 {
@@ -38,17 +42,25 @@ function gotResult(error, results)
 
 function draw()
 {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
+
 
     if(status != "")
     {
+        
+    objectDetector.detect(video, gotResult);
+
         for(i = 0 ; i < objects.length ; i++)
         {
             document.getElementById("status").innerHTML = "Objects Detected";
-            fill("red");
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            fill(r, g, b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%" , objects[i].x , objects[i].y);
-            stroke("red");
+            document.getElementById("Number_of_objects").innerHTML = "No. of objects detected : " + objects.length;
+            stroke(r, g, b);
             noFill();
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
         }
